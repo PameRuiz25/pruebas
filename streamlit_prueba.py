7f7fb054088d36d3089ab2914af432bac449b616
PameRuiz25 <a01423595@itesm.mx>
2024-05-18 14:44:11 -0600

    Update streamlit_prueba.py

--- streamlit_prueba.py
+++ streamlit_prueba.py
@@ -1,10 +1,27 @@
+# 1. Libraries
 import streamlit as st
 import pandas as pd
 import altair as alt
 import plotly.express as px
 
+# 2. Page Configuration
+st.set_page_config(
+    page_title="US Population Dashboard",
+    page_icon="🏂",
+    layout="wide",
+    initial_sidebar_state="expanded")
 
+alt.themes.enable("dark")
+
+# PRUEBA DE QUE FUNCIONA STREAMLIT
 st.write("Streamlit is working!")
+
+# PRUEBA. DISPLAY DASHBOARD
+df = pd.read_csv('data_complete.csv')
+st.write(df)
+
+
+
 
 
 
